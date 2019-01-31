21465ab14f53aaddba06e672edd3b6dc60413f1c
Marcin <marcin.wosik.nauka@gmail.com>
2019-01-31 17:28:56 +0100

    Java

---
+++ c_Programowanie_Obiektowe/c_Metody/Product.java
@@ -0,0 +1,19 @@
+/*
+Utwórz klasę Product dodaj w niej atrybut:
+name
+Dodaj metody setName (ustawiającą name ) oraz getName (zwracającą name ).
+*/
+
+package c_Metody;
+
+public class Product {
+	private String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
